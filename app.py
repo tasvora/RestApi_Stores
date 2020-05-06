9ce68560d3f8cc4e313e976fc55a9933225d2483
tasneem vora <tasneemd18@gmail.com>
2020-05-06 15:14:09 -0400

    Added PostgreSQL environment variable and psycopg2.

--- app.py
+++ app.py
@@ -1,3 +1,5 @@
+import os
+
 from flask import Flask
 from flask_restful import Api
 from flask_jwt import JWT
@@ -10,7 +12,10 @@ from resources.store import Store, StoreList
 from db import db
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] ='sqlite:///data.db'
+#app.config['SQLALCHEMY_DATABASE_URI'] ='sqlite:///data.db'
+#Changing it to the heroku database url but if we wanna test locally it won't be available
+#hence we give two params to get if first not found second will be used
+app.config['SQLALCHEMY_DATABASE_URI']= os.environ.get('DATABASE_URL', 'sqlite:///data.db')
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 #app.config['PROPAGATE_EXCEPTIONS'] = True
 app.secret_key = 'taz'
